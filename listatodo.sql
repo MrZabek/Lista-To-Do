3ab2d9fdf1fcc5deca8922e53b92785232083ebe
Marcin Zabrocki <120128110+MrZabek@users.noreply.github.com>
2023-01-05 22:39:13 +0100

    Zadanie 1
    Zmiana code style na zgodne z standardem PSR1/PSR2 za pomocą PHPStorm

--- listatodo.sql
+++ listatodo.sql
@@ -7,9 +7,11 @@
 -- Wersja serwera: 10.4.14-MariaDB
 -- Wersja PHP: 7.4.11
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET
+SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
-SET time_zone = "+00:00";
+SET
+time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -27,26 +29,27 @@ SET time_zone = "+00:00";
 -- Struktura tabeli dla tabeli `zadaniadb`
 --
 
-CREATE TABLE `zadaniadb` (
-  `numer` int(11) NOT NULL,
-  `zadanie` mediumtext CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL,
-  `data` date NOT NULL,
-  `czas` time NOT NULL,
-  `stan` tinyint(1) NOT NULL
+CREATE TABLE `zadaniadb`
+(
+    `numer`   int(11) NOT NULL,
+    `zadanie` mediumtext CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL,
+    `data`    date NOT NULL,
+    `czas`    time NOT NULL,
+    `stan`    tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Zrzut danych tabeli `zadaniadb`
 --
 
-INSERT INTO `zadaniadb` (`numer`, `zadanie`, `data`, `czas`, `stan`) VALUES
-(1, 'Napisz aplikację', '2022-12-31', '00:55:00', 1),
-(2, 'Napisz skrypty', '2022-12-30', '23:57:00', 1),
-(3, 'Ustaw wygląd strony', '2022-12-31', '00:57:00', 1),
-(4, 'Utwórz bazę danych ', '2022-12-30', '15:58:00', 1),
-(5, 'Wypełnij bazę danych rekordami z danymi', '2022-12-31', '22:58:00', 1),
-(6, 'Nauka php laravel oraz programowania obiektowego w PHP', '2022-12-29', '23:59:00', 0),
-(7, 'Zwrócenie wykonanego projektu aplikacji To-Do z wykorzystaniem funkcji CRUD', '2022-12-31', '12:02:00', 0);
+INSERT INTO `zadaniadb` (`numer`, `zadanie`, `data`, `czas`, `stan`)
+VALUES (1, 'Napisz aplikację', '2022-12-31', '00:55:00', 1),
+       (2, 'Napisz skrypty', '2022-12-30', '23:57:00', 1),
+       (3, 'Ustaw wygląd strony', '2022-12-31', '00:57:00', 1),
+       (4, 'Utwórz bazę danych ', '2022-12-30', '15:58:00', 1),
+       (5, 'Wypełnij bazę danych rekordami z danymi', '2022-12-31', '22:58:00', 1),
+       (6, 'Nauka php laravel oraz programowania obiektowego w PHP', '2022-12-29', '23:59:00', 0),
+       (7, 'Zwrócenie wykonanego projektu aplikacji To-Do z wykorzystaniem funkcji CRUD', '2022-12-31', '12:02:00', 0);
 
 --
 -- Indeksy dla zrzutów tabel
@@ -56,7 +59,7 @@ INSERT INTO `zadaniadb` (`numer`, `zadanie`, `data`, `czas`, `stan`) VALUES
 -- Indeksy dla tabeli `zadaniadb`
 --
 ALTER TABLE `zadaniadb`
-  ADD PRIMARY KEY (`numer`);
+    ADD PRIMARY KEY (`numer`);
 
 --
 -- AUTO_INCREMENT dla zrzuconych tabel
@@ -66,7 +69,7 @@ ALTER TABLE `zadaniadb`
 -- AUTO_INCREMENT dla tabeli `zadaniadb`
 --
 ALTER TABLE `zadaniadb`
-  MODIFY `numer` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+    MODIFY `numer` int (11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
