aef5d17513838638f9a29efe865108857be745cb
Marcin Zabrocki <120128110+MrZabek@users.noreply.github.com>
2023-01-09 19:27:50 +0100

    Polecenie 4

--- ustawienia.php
+++ ustawienia.php
@@ -1,7 +1,87 @@
 <?php
 
 session_start();
+class Application
+{
+    private static $instance;
 
+
+    public function getInstance()
+    {
+        if (!isset(static::$instance)) {
+            static::$instance = new static();
+        }
+        return static::$instance;
+    }
+    public function connect()
+    {
+        if (isset(static::$instance))
+            $pol=mysqli_connect('localhost','root','','listatodo');
+        return $pol;
+    }
+
+    public function run()
+    {
+        if (isset(static::$instance)) {
+            echo('
+                <!DOCTYPE HTML>
+                <html LANG="PL">
+                <head>
+                    <meta charset="UTF-8">
+                    <title>Lista To Do</title>
+                    <meta name="author" content="Marcin Zabrocki">
+                    <meta http-equiv="x-ua-compatible" content="IE=EDGE,chrome=1">
+                    <link rel="stylesheet" href="src/style.css">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                </head>
+                <body>
+                    <header>
+                    <div>
+                        <h1><b>Lista zadań do wykonania</b></h1>
+                        <form action="src/ustawienia.php" method="post">
+                            <input type="text" name="polecenie" id="pole" placeholder="Dodaj zadanie"/>
+                            <input type="date" name="data_zadania" id="pole_data"/>
+                            <input type="time" name="czas_zadania" id="pole_czas"/>
+                            <input type="submit" name="przeslij" id="button" value="Zatwierdz"/><br>
+                        </form>
+                    </div>');
+            walid();
+            echo('</header>
+                    <main>
+                        <table>
+                            <tr id="naglowek">
+                                <th>Nr.</th>
+                             <th>Zadanie</th>
+                                <th>Wykonać do</th>
+                                <th>Akcje</th>
+                                <form method="$_GET" action="src/ustawienia.php">
+                                    <th><input type="submit" value="delete" name="row_delete_multiple" id="delete"/></th>');
+            dane();
+            echo('
+                            </tr>
+                            </form>
+                        </table>
+                    </main>
+                    <footer>
+                        <div>
+                            <form method="POST" action="src/ustawienia.php">
+                                <input type="text" name="wyszpolecenie" id="wyszpole"
+                                        placeholder="Wyszukaj polecenie"></input>
+                                <input type="date" name="wyszdata_zadania"
+                                        id="wyszpole_data"></input>
+                                <input type="submit" name="wyszprzeslij" id="btnwyszukaj"
+                                        value="Wyszukaj" title="Aby wrócić do wszystkich zadań naciśnij ponownie"><br>
+                            </form>
+                        </div>
+                        <div>
+                            <h5>Author: Marcin Zabrocki</h5>
+                        </div>
+                    </footer>
+                </body>
+            </html>');
+        }
+    }
+}
 //Struktura obiektów
 class zadania
 {
@@ -43,11 +123,11 @@ class zadania
                  <td class='akcje'>";
 
         if ($this->stan_zad == 0) {
-            echo "<a href='ustawienia.php?zrobione=$this->id_zad'  class='dozrobienia'>Do zrobienia</a>";
+            echo "<a href='src/ustawienia.php?zrobione=$this->id_zad'  class='dozrobienia'>Do zrobienia</a>";
         } else {
-            echo "<a href='ustawienia.php?dozrobienia=$this->id_zad' value='$this->numer_zad' class='zrobione'>Zrobione</a>";
+            echo "<a href='src/ustawienia.php?dozrobienia=$this->id_zad' value='$this->numer_zad' class='zrobione'>Zrobione</a>";
         }
-        echo "<a href='ustawienia.php?del=$this->id_zad' class='usun'>X</a></td>";
+        echo "<a href='src/ustawienia.php?del=$this->id_zad' class='usun'>X</a></td>";
         echo "<td><input class='check' type='checkbox' name='usun_wiele[]' value='$this->id_zad'></td>";
         echo "</tr>";
 
@@ -101,7 +181,8 @@ function dodaj()
         $_SESSION['alert'] = '<h4>Wprowadz polecenie</h4>';
     }
     mysqli_close($conn);
-    header("location: index.php");
+    $pow = router::powrot();
+    echo $pow;
 }
 
 function walid()
@@ -123,7 +204,8 @@ function usun()
     $query = "DELETE FROM `zadaniadb` WHERE `numer` IN($format_id);";
     mysqli_query($conn, $query);
     mysqli_close($conn);
-    header("location:index.php");
+    $pow = router::powrot();
+    echo $pow;
 }
 
 function deleteonce()
@@ -133,7 +215,8 @@ function deleteonce()
     $id = $_GET['del'];
     mysqli_query($conn, "DELETE FROM `zadaniadb` WHERE `numer` =$id;");
     mysqli_close($conn);
-    header("location: index.php");
+    $pow = router::powrot();
+    echo $pow;
 }
 
 function stan()
@@ -151,7 +234,8 @@ function stan()
             "UPDATE `zadaniadb` SET `stan` = '1' WHERE `numer` = $id;");
     }
     mysqli_close($conn);
-    header("location: index.php");
+    $pow = router::powrot();
+    echo $pow;
 }
 
 //Wyszukiwanie na podstawie zadania lub daty
@@ -172,19 +256,34 @@ function wyszukaj()
         $_SESSION['zapytanie']
             = "SELECT * FROM `zadaniadb` WHERE `data` LIKE '$element';";
     }
-    header("location: index.php");
+    $pow = router::powrot();
+    echo $pow;
 }
 
+class router
+{
 
-if (isset($_GET['zrobione']) || isset($_GET['dozrobienia'])) {
-    stan();
-} elseif (isset($_GET['del'])) {
-    deleteonce();
-} elseif (isset($_GET['row_delete_multiple'])) {
-    usun();
-} elseif (isset($_POST['przeslij'])) {
-    dodaj();
-} elseif (isset($_POST['wyszprzeslij'])) {
-    wyszukaj();
+    public function przekierowanie()
+    {
+        if (isset($_GET['zrobione']) || isset($_GET['dozrobienia'])) {
+            stan();
+        } elseif (isset($_GET['del'])) {
+            deleteonce();
+        } elseif (isset($_GET['row_delete_multiple'])) {
+            usun();
+        } elseif (isset($_POST['przeslij'])) {
+            dodaj();
+        } elseif (isset($_POST['wyszprzeslij'])) {
+            wyszukaj();
+        }
+    }
+
+    public function powrot()
+    {
+        header("location:\Lista-To-Do\index.php");
+    }
 }
+
+$akcja = router::przekierowanie();
+echo $akcja;
 ?>
