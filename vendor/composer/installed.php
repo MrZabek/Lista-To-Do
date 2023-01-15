bceb3816ac0a624ecc9d1c9c421cc27b694bcea6
Marcin Zabrocki <120128110+MrZabek@users.noreply.github.com>
2023-01-15 16:37:04 +0100

    all

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'app/composer',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '178fcfe9f72f2e6fbb11daa1143bcb8567d10497',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'app/composer' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '178fcfe9f72f2e6fbb11daa1143bcb8567d10497',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
